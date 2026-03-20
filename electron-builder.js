6f56de9f1df73e2e8da37b3d2460044cc260b535
WithoutWout <admin@wirewave.net>
2026-03-21 00:56:33 +0100

    ci: remove electron-builder.js, plain config only, no notarize for now

--- electron-builder.js
+++
@@ -1,9 +0,0 @@
-const base = require("./package.json").build
-
-const config = { ...base }
-
-if (process.env.APPLE_TEAM_ID) {
-  config.mac = { ...base.mac, notarize: { teamId: process.env.APPLE_TEAM_ID } }
-}
-
-module.exports = config
